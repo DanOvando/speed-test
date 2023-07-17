606520dd1637ced3b05dc1bb24754a27debaf1da
DanOvando <danovan@uw.edu>
2023-07-17 12:30:11 -0700

    passing to POS

--- speed_test.R
+++ speed_test.R
@@ -3,22 +3,22 @@ library(tictoc)
 library(rstan)
 library(cmdstanr)
 library(sf)
-# library(TMB)
 library(sdmTMB)
 library(INLA)
 library(rnaturalearth)
-# library(VAST)
+library(VAST)
 library(cmdstanr)
 check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
 library(posterior)
 library(bayesplot)
+library(here)
 color_scheme_set("brightblue")
 tic()
 
 # bayesian test -----------------------------------------------------------
 
 # Generate some fake data
-n <- 250000
+n <- 25000
 k <- 20
 X <- matrix(rnorm(n * k), ncol = k)
 y <- rbinom(n, size = 1, prob = plogis(3 * X[,1] - 2 * X[,2] + 1))
@@ -28,10 +28,13 @@ mod_cl <- cmdstan_model("logistic.stan")
 
 fit_cl <- mod_cl$sample(data = mdata, chains = 4, parallel_chains = 4, refresh = 100)
 
+model <- stan_demo(model = 516, chains = 4, cores = 4)
+
+
+
 # mixed effects test ------------------------------------------------------
 
 # Load package
-library(VAST)
 
 # load data set
 # see `?load_example` for list of stocks with example data 
@@ -42,7 +45,7 @@ example = load_example( data_set="EBS_pollock" )
 settings = make_settings( n_x = 100, 
                           Region = example$Region, 
                           purpose = "index2", 
-                          bias.correct = TRUE )
+                          bias.correct = FALSE )
 
 # Run model
 fit = fit_model( settings = settings, 
@@ -55,16 +58,24 @@ fit = fit_model( settings = settings,
 # Plot results
 plot( fit )
 
-# mapping test ------------------------------------------------------------
 mesh <- make_mesh(pcod, xy_cols = c("X", "Y"), cutoff = 10)
-pcod$year_scaled <- as.numeric(scale(pcod$year))
-fit <- sdmTMB(
-  density ~ s(depth, k = 5) + year_scaled,
-  spatial_varying = ~ year_scaled, 
-  data = pcod, mesh = mesh, 
-  time = "year",
-  family = tweedie(link = "log"),
-  spatiotemporal = "off"
-)
+
+d <- subset(pcod, year == 2017)
+pcod_spde <- make_mesh(d, c("X", "Y"), cutoff = 30)
+m <- sdmTMB(density ~ 0 + depth_scaled + depth_scaled2,
+            data = d, mesh = pcod_spde, family = tweedie(link = "log"))
+p <- predict(m, newdata = qcs_grid)
+
+
+# mapping test ------------------------------------------------------------
+
+
+eezs <-
+  sf::read_sf(here("World_EEZ_v11_20191118_LR", "eez_v11_lowres.shp")) 
+
+eezs |> 
+  ggplot() + 
+  geom_sf()
 
 toc()
+
